4d174eb67d863f45da0714b435d13d958e1fb40e
Dmitrii Lukashev <seo@chillicode.ru>
2021-01-18 18:40:45 +0300

    2

--- net.pullquest/src/test/java/net/pullquest/FirstTest.java
+++ net.pullquest/src/test/java/net/pullquest/FirstTest.java
@@ -1,13 +1,17 @@
 package net.pullquest;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
-public class FirstTest extends WebDriverSettings{
+public class FirstTest extends WebDriverSettings {
     @Test
 
-    public void firstTest () {
-        driver.get("https://dev.pullquest.net/compilations");
+    public void firstTest() {
+        driver.get("https://dev.pullquest.net");
+
+        WebElement header = driver.findElement(By.className("_HRzlow"));
+        header.findElement(By.cssSelector("[href=\"/login\"]")).click();
+
     }
-
-
 }
